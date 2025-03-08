a3f4816ce6c48fdab29c77f6fb4bf06b1b307acc
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-03-08 15:49:24 -0500

    Queries Quality and Percentages

--- Queries_Qualit_Percentage.sql
+++ Queries_Qualit_Percentage.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
 
 SELECT QUERY_NAME, ROUND(AVG(RATING/POSITION),2) AS QUALITY, ROUND(AVG(IF(RATING<3, 1,0) *100),2) AS POOR_QUERY_PERCENTAGE
 FROM QUERIES
