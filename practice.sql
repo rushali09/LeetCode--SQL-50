778ffedf45c04d0965062488e4533a155623f6e8
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-04-20 05:07:18 -0400

    Consecutive Numbers

--- practice.sql
+++ practice.sql
@@ -259,6 +259,21 @@ FROM TRIANGLE
 --
 
 
+--180. Consecutive Numbers
+SELECT L1.NUM AS ConsecutiveNums
+FROM LOGS L1, LOGS L2, LOGS L3
+WHERE 
+L1.ID = L2.ID -1 AND L2.ID = L3.ID -1
+AND 
+L1.NUM = L2.NUM AND L2.NUM = L3.NUM
+--
+
+
+
+
+
+
+
 
 
 
