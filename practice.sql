404d6db5221396bf901d243b88ecfb76bb0cdbc4
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-05-11 16:30:33 -0400

    626. Exchange Seats

--- practice.sql
+++ practice.sql
@@ -684,6 +684,37 @@ LIMIT 1)
 
 
 
+--626. Exchange Seats
+SELECT 
+    CASE 
+        WHEN ID = (SELECT MAX(ID) FROM SEAT) AND MOD(ID,2) = 1
+        THEN ID
+
+        WHEN MOD(ID,2) = 1
+        THEN ID+1
+
+        ELSE (ID-1)
+    END AS ID, STUDENT
+FROM SEAT
+ORDER BY ID 
+--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
