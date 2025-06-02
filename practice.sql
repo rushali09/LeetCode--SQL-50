18ad711008a78a1c39a88ed5a42eabcd33dccdfc
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-06-01 21:35:23 -0400

    Average processing time

--- practice.sql
+++ practice.sql
@@ -1009,7 +1009,13 @@ WHERE BONUS < 1000 OR BONUS IS NULL
 
 
 
-
+--
+SELECT s.machine_id, ROUND(AVG(e.timestamp-s.timestamp), 3) AS processing_time
+FROM Activity s JOIN Activity e ON
+    s.machine_id = e.machine_id AND s.process_id = e.process_id AND
+    s.activity_type = 'start' AND e.activity_type = 'end'
+GROUP BY s.machine_id;
+--
 
 
 
