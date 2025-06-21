55142ad6fecb4ba62f5172a0e9ca1230d3b09e34
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-06-21 00:10:52 -0400

    seat

--- practice.sql
+++ practice.sql
@@ -1335,3 +1335,29 @@ order by employee_id
 
 
 
+SELECT 
+    CASE 
+        WHEN ID = (SELECT MAX(ID) FROM SEAT) AND MOD(ID,2) =1 
+        THEN ID
+     
+        WHEN MOD(ID,2) = 1
+        THEN ID+1
+
+        ELSE (ID-1)
+    END AS ID, STUDENT
+FROM SEAT
+ORDER BY ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
